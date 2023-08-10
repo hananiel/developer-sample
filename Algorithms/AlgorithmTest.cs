2a3878524c315d73f8dff875153686030e15376e
Brandon Say <brandonsay528@gmail.com>
2023-08-10 14:41:49 -0700

    Completed GetFactorial() and added to unit test to make sure math adds up

--- Algorithms/AlgorithmTest.cs
+++ Algorithms/AlgorithmTest.cs
@@ -4,10 +4,15 @@ namespace DeveloperSample.Algorithms
 {
     public class AlgorithmTest
     {
-        [Fact(Skip="Not implemented")]
+        [Fact]
         public void CanGetFactorial()
         {
+            Assert.Equal(1, Algorithms.GetFactorial(1));
+            Assert.Equal(2, Algorithms.GetFactorial(2));
+            Assert.Equal(6, Algorithms.GetFactorial(3));
             Assert.Equal(24, Algorithms.GetFactorial(4));
+            Assert.Equal(120, Algorithms.GetFactorial(5));
+            Assert.Equal(3628800, Algorithms.GetFactorial(10));
         }
 
         [Fact(Skip="Not implemented")]
--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -4,7 +4,22 @@ namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
+        public static int GetFactorial(int n)
+        {
+            int returnValue = n;
+            /*
+                decrement n until it reaches 1 
+                each time multiply returnValue by i
+                ex if n = 5 , returnValue=5 ,  i = 4 , the final iteration will look like
+                returnValue*4*3*2*1
+                resulting in 120
+             */
+            for (int i = n-1; i >0; i--)
+            {
+                returnValue *= i;
+            }
+            return returnValue;
+        }
 
         public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
     }
