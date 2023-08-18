f32299c0c741f17c86c21f94cba9d3d755e1062b
Brandon Say <brandonsay528@gmail.com>
2023-08-18 15:25:02 -0700

    Modified test based on new understanding of Xunit tests

--- Container/ContainerTest.cs
+++ Container/ContainerTest.cs
@@ -1,5 +1,6 @@
 using DeveloperSample.ClassRefactoring;
 using System;
+using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using Xunit;
 
@@ -28,43 +29,14 @@ namespace DeveloperSample.Container
         public void MakeSureImplementable()
         {
             var container = new Container();
-            try
-            {
-                container.Bind(typeof(IContainerTestInterface), typeof(Swallow));
-                Assert.Fail("Bind did not pass test");
-            }
-            catch (InvalidCastException e)
-            {
-                return;
-            }
-            catch (Exception e)
-            {
-                Assert.Fail(e.Message);
-                throw;
-            }
-
+            Assert.Throws<InvalidCastException>(() => container.Bind(typeof(IContainerTestInterface), typeof(Swallow)));
         }
 
         [Fact]
         public void MakeSureBound()
         {
             var container = new Container();
-            try
-            {
-                var testInstance = container.Get<IContainerTestInterface>();
-                Assert.Fail("Get did not pass test");
-
-            }
-            catch (ContainerBindingNotFoundException e)
-            {
-                return;
-            }
-            catch (Exception e)
-            {
-                Assert.Fail(e.Message);
-                throw;
-            }
-
+            Assert.Throws<KeyNotFoundException>(() => container.Get<IContainerTestInterface>());
         }
     }
 }
