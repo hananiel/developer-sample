9dd11a598399c2cc1a221c0f396679a1ae6693b9
Brandon Say <brandonsay528@gmail.com>
2023-08-18 15:24:40 -0700

    modified container based on feedback

--- Container/Container.cs
+++ Container/Container.cs
@@ -15,23 +15,29 @@ namespace DeveloperSample.Container
         public void Bind(Type interfaceType, Type implementationType)
         {
             if (!interfaceType.IsAssignableFrom(implementationType))
-                throw new InvalidCastException($@"Type:{implementationType} does not implement {interfaceType}");
-            if(containerBindings.ContainsKey(interfaceType))
-                containerBindings[interfaceType] = implementationType;
-            containerBindings.Add(interfaceType, implementationType);
+                throw new InvalidCastException($@"Type:{implementationType.FullName} does not implement {interfaceType.FullName}");
+            containerBindings[interfaceType] = implementationType;
+           // if(containerBindings.ContainsKey(interfaceType))
+              //  containerBindings[interfaceType] = implementationType;
+           // containerBindings.Add(interfaceType, implementationType);
         }
         /// <summary>
-        /// Returns binding for type
+        /// 
         /// </summary>
-        /// <typeparam name="T">Interface Type</typeparam>
-        /// <returns>Implementation Type From Bind</returns>
-        /// <exception cref="ContainerBindingNotFoundException">Throws error of binding is not found</exception>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="KeyNotFoundException">Returned when bind is not called prior to get</exception>
+        /// <exception cref="InvalidCastException">implementationType does not implement interfaceType </exception>
         public T Get<T>()
         {
             Type getType = typeof(T);
-            if (containerBindings.ContainsKey(getType))
-                return (T)Activator.CreateInstance(containerBindings[getType]);
-            throw new ContainerBindingNotFoundException(getType);
+            Type containerType;
+            containerBindings.TryGetValue(getType, out containerType);
+            if (containerType == null)
+                throw new KeyNotFoundException($@"Could not find Type:{getType.FullName} in container; make sure to call .Bind() first");
+            if (!getType.IsAssignableFrom(containerType))
+                throw new InvalidCastException($@"Type:{containerType.FullName} does not implement {getType.FullName}");
+            return (T)Activator.CreateInstance(containerType);
 
         }
     }
